9032a8a6409f8359a3dd0863c6249d7847e756b1
PeterKurtz <peterjohnkurtz@gmail.com>
2025-04-05 22:31:08 -0600

    Implemented more for printing menu.

--- printChoices.py
+++ printChoices.py
@@ -8,13 +8,35 @@ def addSpaceAndBorders(statement, editableArea, bufferSpace):
 
     return lineToPrint
 
+def createRequest(request, editableArea, bufferSpace):
+    requestLines = []
+    requestCheck = request
+
+    while len(requestCheck) > editableArea:
+        indexToCheck = findIndex(requestCheck, editableArea)
+        correctRequest = requestCheck[:indexToCheck]
+        correctRequest = addSpaceAndBorders(correctRequest, editableArea, bufferSpace)
+        requestLines.append(correctRequest)
+        requestCheck = requestCheck[indexToCheck + 1:]
+    requestCheck = addSpaceAndBorders(requestCheck, editableArea, bufferSpace)
+
+    requestLines.append(requestCheck)
+
+    return requestLines
+
+
+def findIndex(request, indexToCheck):
+    while request[indexToCheck] != ' ':
+        indexToCheck = indexToCheck - 1
+
+    return indexToCheck
 
 
 
 def printChoices(title, request, editableArea, bufferSpace, choices = []):
     linesToPrint = []
 
-    totalArea = editableArea + bufferSpace
+    totalArea = editableArea + bufferSpace*2
 
     topAndBottomBorder = '+' + '='*totalArea + '+'
     fillerLayer = '|' + ' '*totalArea + '|'
@@ -22,7 +44,15 @@ def printChoices(title, request, editableArea, bufferSpace, choices = []):
     titleUnderline = title
     titleLine = addSpaceAndBorders(titleUnderline, editableArea, bufferSpace)
     linesToPrint = [topAndBottomBorder, fillerLayer, titleLine, fillerLayer]
+    if len(request) > 0:
+        requestLines = createRequest(request, editableArea, bufferSpace)
+        linesToPrint += requestLines
+
+    seperatorLine = addSpaceAndBorders('--', editableArea, bufferSpace)
+
+    linesToPrint.append(seperatorLine)
+
     for line in linesToPrint:
         print(line)
 
-printChoices("test", "test", 40, 3)
+printChoices("test", "test is a test they are the people this is a long test", 20, 3)
